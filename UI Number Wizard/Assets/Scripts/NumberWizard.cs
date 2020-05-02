a3d037bf02f887f82d8c882f57b1ef53b6e24bb2
Harpreet S. Mahli <mahli.harpreet@yahoo.com>
2020-05-02 13:01:17 -0700

    Finished the game, now need to just add some polishing, and create a executable

---
+++ UI Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    public void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min= guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess -1;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        //Because now the variables aren't hard coded, but rather intialized outside of the code, the guess can't be 
+        //hardcoded as well, needs to now calculate whatever int it gets for min and max
+        guess = Random.Range(min, max + 1);
+
+        //Need to convert the int to a string cause our text field is looking for a string to display, not a int
+        guessText.text = guess.ToString();
+    }
+}
---
+++ UI Number Wizard/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Need to use the namespace that will let us manage our scenes
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        //declared a integer that tracks where we currently on
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        //Bring up the next scene
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
